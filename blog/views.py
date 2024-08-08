54734600c41893e8594a36a40d89ff0760564a64
SnayderCJ <Jcedenoj5@unemi.edu.ec>
2024-08-08 00:02:11 -0500

    Update

--- blog/views.py
+++ blog/views.py
@@ -7,10 +7,10 @@ def render_posts(request):
     posts = Post.objects.all()
     return render(request, 'posts.html', {
         'posts': posts
-    })
+    })  
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'post_detail.html', {
         'post': post
-    })
+    }) 
